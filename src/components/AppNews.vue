d47de8445028af990d4a7e4f002c2b34de97420f
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-04-24 01:11:48 +0500

    fix error

--- src/components/AppNews.vue
+++ src/components/AppNews.vue
@@ -5,7 +5,6 @@ import { formatTime } from '@/composables/formatTime'
 import BaseButton from '@/components/BaseButton.vue'
 import AppLoader from '@/components/AppLoader.vue'
 import LoaderWrapper from '@/components/LoaderWrapper.vue'
-import { idText } from 'typescript'
 
 const allNews = ref()
 const isLoading = ref(true)
@@ -16,12 +15,12 @@ onMounted(async () => {
   allNews.value = data.data.data.body
   isLoading.value = false
   console.log(data.data.data.body)
-  setTimeout(() => {
-    if (isLoading.value) {
-      isLoading.value = false
-    }
-  }, 5000)
 })
+setTimeout(() => {
+  if (isLoading.value) {
+    isLoading.value = false
+  }
+}, 4000)
 </script>
 
 <template>
