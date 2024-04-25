5ece69d45f4f1cd7c8ea1f79b475f8140c248cc6
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-04-25 23:29:02 +0500

    Fix LoaderWrapper z-index

--- src/components/LoaderWrapper.vue
+++ src/components/LoaderWrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed z-[100] bg-[#00000099] flex items-center justify-center min-h-screen w-full top-0 right-0 left-0 bottom-0"
+    class="fixed z-[2000] bg-[#00000099] flex items-center justify-center min-h-screen w-full top-0 right-0 left-0 bottom-0"
   >
     <slot />
   </div>
